aed7c51ffc576aa61300a86a304bc02705e3fcd4
Struan Murray <37640681+Struan-Murray@users.noreply.github.com>
2018-03-22 03:27:50 +0000

    Switch to C code

---
+++ factorial.c
@@ -0,0 +1,28 @@
+#include "factorial.h"
+
+uintmax_t factorial(uint16_t factor){
+	
+	const uintmax_t errorValue{0};
+	uint8_t maxFactor[]{5, 8, 10, 12, 14, 16, 18, 20}; //128-34
+	
+	if(factor < 0 || factor > maxFactor[sizeof(uintmax_t)-1]){
+		return errorValue;
+	}
+	else{return factorialMath(factor);}
+}
+
+uintmax_t factorial(uint16_t factor, uintmax_t returnValue){
+	
+	const uintmax_t errorValue{returnValue};
+	uint8_t maxFactor[]{5, 8, 10, 12, 14, 16, 18, 20}; //128-34
+	
+	if(factor < 0 || factor > maxFactor[sizeof(uintmax_t)-1]){
+		return errorValue;
+	}
+	else{return factorialMath(factor);}
+}
+
+uintmax_t factorialMath(uint16_t factor){
+	
+	return (factor == 0 || factor == 1) ? 1 : factor * factorialMath(factor - 1);
+}
--- factorial.h
+++ factorial.h
@@ -10,17 +10,17 @@
  */
 uintmax_t factorial(uint16_t factor);
 
-/* Takes a value through first argument and returns via an unsigned integer. 
- * Return value is set via the second argument for invalid entry. 
+/* Takes a value through first argument and returns via an unsigned integer.
+ * Return value is set via the second argument for invalid entry.
  * (Too large, not a number or negative).
  * Runs "factorialMath" for its calculations.
  */
-uintmax_t factorial(uint16_t factor, bool retValue);
+uintmax_t factorial(uint16_t factor, uintmax_t returnValue);
 
 
 /* Does the calculations for the factorial.
  * Takes a value through first argument and returns via an unsigned integer.
- * Can also be used on its own without the above checks but will cause errors if input is invalid.
+ * Can also be used on its own without the above checks but will cause errors * if input is invalid.
  */
 uintmax_t factorialMath(uint16_t factor);
 
