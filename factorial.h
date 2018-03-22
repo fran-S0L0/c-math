0471a12da69c700a07458cadf04c55f3fd40388c
Struan Murray <37640681+Struan-Murray@users.noreply.github.com>
2018-03-22 00:13:00 +0000

    Update factorial.h

--- factorial.h
+++ factorial.h
@@ -6,13 +6,22 @@
 /* Takes a value through first argument and returns via an unsigned integer.
  * Return value is 0 for invalid entry.
  * (Too large, not a number or negative).
+ * Runs "factorialMath" for its calculations.
  */
 uintmax_t factorial(uint16_t factor);
 
 /* Takes a value through first argument and returns via an unsigned integer. 
  * Return value is set via the second argument for invalid entry. 
  * (Too large, not a number or negative).
+ * Runs "factorialMath" for its calculations.
  */
 uintmax_t factorial(uint16_t factor, bool retValue);
+
+
+/* Does the calculations for the factorial.
+ * Takes a value through first argument and returns via an unsigned integer.
+ * Can also be used on its own without the above checks but will cause errors if input is invalid.
+ */
+uintmax_t factorialMath(uint16_t factor);
 
 #endif
